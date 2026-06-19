cca57e7ae34d25b6f7f94b71ca0f1efb79a2c161
sa-ban777 <sabano@niwadenki.co.jp>
2026-06-19 12:37:50 +0900

    Fix channel shape orientation

---
+++ channel-orientation.js
@@ -0,0 +1,1 @@
+(function(){var d=window.BEAM_XLSM_DATA;if(!d||!d.shapes)return;d.shapes.forEach(function(s){if((s.shapeKind==='溝形鋼'||s.jisKind==='溝形鋼')&&s.sectionType==='コの字'){s.sectionType='コの字2';}});})();
